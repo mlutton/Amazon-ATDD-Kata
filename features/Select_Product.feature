b11197cfd47097c19d94a1b2140fdb11fd75c31a
Michael Lutton <mlutton@netjets.com>
2013-07-24 15:51:40 -0400

    Added logic around validating that the product detail page shows a ttile, price, and customer review

---
+++ features/Select_Product.feature
@@ -0,0 +1,8 @@
+Feature: Select A Product
+  As a Amazon customer
+  I want to view a product page
+  So that I can learn more about the product
+
+Scenario: Product Details Should Include Key Fields
+  When I am on a product detail page
+  Then I should see a price, description, and reviews
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -4,3 +4,11 @@ end
 Then(/^I should see a list of available products$/) do
   on(AmazonHomePage).text.should include 'Kindle'
 end
+When(/^I am on a product detail page$/) do
+  visit ProductDetailPage
+end
+Then(/^I should see a price, description, and reviews$/) do
+  on(ProductDetailPage).productTitle.should include 'Kindle'
+  on(ProductDetailPage).price.should include '$69'
+  on(ProductDetailPage).customer_review?.should be_true
+end
