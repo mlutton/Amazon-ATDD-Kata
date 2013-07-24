80bcefc5d5587aabd287f573c92996e5238454b4
Michael Lutton <mlutton@netjets.com>
2013-07-24 15:51:40 -0400

    Added logic around validating that the product detail page shows a ttile, price, and customer review

---
+++ features/support/pages/product_detail_page.rb
@@ -0,0 +1,8 @@
+class ProductDetailPage
+  include PageObject
+  page_url ('http://www.amazon.com/gp/product/B007HCCNJU/ref=s9_simh_gw_p349_d2_i3')
+
+  span(:productTitle, :id => 'btAsinTitle')
+  div(:price, :id => 'priceBlock')
+  div(:customer_review, :class => 'reviews', :index => 0)
+end
